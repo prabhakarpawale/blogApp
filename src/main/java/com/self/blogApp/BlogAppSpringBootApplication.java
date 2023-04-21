87bd92ecd0fdc892bdedb4c864ffc01cd4fc217b
Prabhakar Pawale <prabhakarpawale1406@gmail.com>
2023-04-21 13:52:51 +0530

    Initial Project Setup

---
+++ src/main/java/com/self/blogApp/BlogAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.self.blogApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/self/blogApp/BlogAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.blogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
